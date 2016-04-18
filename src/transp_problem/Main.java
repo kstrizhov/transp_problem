68b45063dfdc98dccb7bd79d3bc9f783c075af21
kstrizhov <kstrizhov@gmail.com>
2016-04-18 20:27:53 +0300

    some garbage to make notes of ideas

--- src/transp_problem/Main.java
+++ src/transp_problem/Main.java
@@ -2,29 +2,43 @@ package transp_problem;
 
 import java.util.ArrayList;
 
+import Jama.Matrix;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//DataImporter.createData();
+		DataImporter.createData();
 
 		ArrayList<Mine> producers = new ArrayList<Mine>();
-		producers.add(new Mine(1));
-		producers.add(new Mine(2));
-		producers.add(new Mine(3));
+		producers.add(new Mine(11));
+		producers.add(new Mine(11));
+		producers.add(new Mine(8));
 		producers.add(new Mine(4));
-		
+
 		ArrayList<ConsumptionPoint> consumers = new ArrayList<ConsumptionPoint>();
 		consumers.add(new ConsumptionPoint(5));
-		consumers.add(new ConsumptionPoint(1));
-		consumers.add(new ConsumptionPoint(2));
-		consumers.add(new ConsumptionPoint(2));
-		
-		int[][] plan = Planner.createBasicPlan(producers, consumers);
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++)
-				System.out.print(plan[i][j]);
-			System.out.println("");
+		consumers.add(new ConsumptionPoint(9));
+		consumers.add(new ConsumptionPoint(9));
+		consumers.add(new ConsumptionPoint(7));
+
+		IntMatrix plan = Planner.createBasicPlan(producers, consumers);
+		plan.print();
+
+		double[] x = { 5, 6, 0, 0, 0, 3, 8, 0, 0, 0, 1, 7 };
+		double[] c1 = { 7, 8, 5, 3, 2, 4, 5, 9, 6, 3, 1, 2 };
+
+		Matrix X = (new Matrix(x, 4)).transpose();
+		Matrix C1 = (new Matrix(c1, 4)).transpose();
+
+		Planner.MatrixSet set = Planner.getCoeffMatrix(X, C1);
+
+		Matrix potentials = null;
+		try {
+			potentials = Planner.getPotentialVector(set);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		potentials.print(potentials.getColumnDimension(), 2);
 	}
 }
--- src/transp_problem/Planner.java
+++ src/transp_problem/Planner.java
@@ -1,17 +1,17 @@
 package transp_problem;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import Jama.Matrix;
 
 public class Planner {
 
-	public static int[][] createBasicPlan(List<Mine> producers, List<ConsumptionPoint> consumers) {
+	public static IntMatrix createBasicPlan(List<Mine> producers, List<ConsumptionPoint> consumers) {
 		int numOfProducers = producers.size();
 		int numOfConsumers = consumers.size();
 
-		int[][] planMatrix = new int[numOfProducers][numOfConsumers];
-		for (int i = 0; i < numOfProducers; i++)
-			for (int j = 0; j < numOfConsumers; j++)
-				planMatrix[i][j] = -1;
+		IntMatrix planMatrix = new IntMatrix(numOfProducers, numOfConsumers, -1);
 
 		int numOfSteps = numOfProducers + numOfConsumers - 1;
 
@@ -24,18 +24,18 @@ public class Planner {
 			int consumption = consumers.get(j).getConsumption();
 
 			if (production < consumption) {
-				planMatrix[i][j] = production;
-				consumers.get(j).setConsumption(consumption-production);
+				planMatrix.set(i, j, production);
+				consumers.get(j).setConsumption(consumption - production);
 				producers.get(i).setProduction(0);
 				for (int t = j + 1; t < numOfConsumers; t++)
-					planMatrix[i][t] = 0;
+					planMatrix.set(i, t, 0);
 				i++;
 			} else {
-				planMatrix[i][j] = consumption;
+				planMatrix.set(i, j, consumption);
 				producers.get(i).setProduction(production - consumption);
 				consumers.get(j).setConsumption(0);
 				for (int t = i + 1; t < numOfProducers; t++)
-					planMatrix[t][j] = 0;
+					planMatrix.set(t, j, 0);
 				j++;
 			}
 		}
@@ -43,9 +43,80 @@ public class Planner {
 		return planMatrix;
 	};
 
-	private void optimizeBasicPlan() {
+	public static class MatrixSet {
+		private Matrix A;
+		private Matrix B;
 
-		// ...
+		private MatrixSet(Matrix A, Matrix B) {
+			this.A = A;
+			this.B = B;
+		}
+	}
 
-	};
+	public static MatrixSet getCoeffMatrix(Matrix X, Matrix C) {
+		int numOfRows = X.getRowDimension();
+		int numOfColumns = X.getColumnDimension();
+		int numOfConditions = numOfRows + numOfColumns;
+
+		Matrix A = new Matrix(numOfConditions, numOfConditions);
+		Matrix B = new Matrix(numOfConditions, 1);
+
+		A.set(0, 0, 1);
+		B.set(0, 0, 0);
+
+		int k = 1;
+
+		for (int i = 0; i < X.getRowDimension(); i++)
+			for (int j = 0; j < X.getColumnDimension(); j++)
+				if (X.get(i, j) != 0) {
+					double uCoeff = -1;
+					double vCoeff = 1;
+					A.set(k, i, uCoeff);
+					A.set(k, j + numOfRows, vCoeff);
+					double cost = C.get(i, j);
+					B.set(k, 0, cost);
+					k++;
+				}
+		
+		A.print(A.getColumnDimension(), 2);
+		B.print(B.getColumnDimension(), 2);
+
+		return new MatrixSet(A, B);
+	}
+
+	public static Matrix getPotentialVector(MatrixSet set) throws Exception {
+		return solveWithKramer(set.A, set.B);
+	}
+
+	/*------Kramer's method-------*/
+
+	private static Matrix solveWithKramer(Matrix A, Matrix B) throws Exception {
+
+		if (A.getRowDimension() != B.getRowDimension())
+			throw new Exception("Numbers of rows of A and B are not equal!");
+
+		int numOfEquations = A.getRowDimension();
+
+		Matrix X = new Matrix(numOfEquations, 1);
+		ArrayList<Matrix> matrixList = new ArrayList<Matrix>();
+
+		for (int i = 0; i < numOfEquations; i++) {
+			Matrix D = A.copy();
+			for (int j = 0; j < numOfEquations; j++) {
+				D.set(j, i, B.get(j, 0));
+			}
+			matrixList.add(D);
+		}
+
+		for (int i = 0; i < numOfEquations; i++) {
+			double solution = matrixList.get(i).det() / A.det();
+			X.set(i, 0, solution);
+			System.out.println(matrixList.get(i).det());
+			int[] rows = {1,2,3,4,5,6};
+			int[] cols = {1,2,3,4,5,6};
+			System.err.println("DET: " + A.getMatrix(rows,cols).det());
+		}
+
+		return X;
+	}
 }
